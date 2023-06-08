8088d8570f9a34ae4e9531085dd743ac6699496e
Anil <prajapatanil200@gmail.com>
2023-06-08 16:45:17 +0530

    add Api request successfully

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,35 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
 import axios from "axios";
 
-const apiKey="fe2c1ac7b45875ea5add1a375ec18cbe"
+const apiKey="fe2c1ac7b45875ea5add1a375ec18cbe";
+const url="https://api.themoviedb.org/3";
+const imgUrl="https://image.tmdb.org/t/p/original";
+const upcoming="upcoming";
+const nowPlaying="now_playing";
+const popular= "popular";
+const topRated= "top_rated";
+
+
+
 const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
-const Row = ({
-  title,
-  arr = [
-    {
-      img: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQA_-tL18_rj9zEcjN6n41NEaJm-kRNF9UeOtvksZ4z_OW6jRA9",
-    },
-  ],
-}) => (
+const Row = ({ title, arr = []}) => (
   <div className="row">
     <h2>{title}</h2>
     <div>
-      {arr.map((item) => (
-        <Card img={item.img} />
+      {arr.map((item,index) => (
+        <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
       ))}
     </div>
   </div>
 );
 const Home = () => {
+
+  const[upcomingMovies ,setUpcomingMovies]= useState([]);
+  const[playingMovies ,setPlayingMovies]= useState([]);
+  const[popularMovies ,setPopularMovies]= useState([]);
+  const[topRatedMovies ,setTopRatedMovies]= useState([]);
+
+useEffect(()=>{
+
+const fetchUpcoming= async()=>{
+const{data:{results}}= await axios.get(`${url}/movie/${upcoming}?api_key=${apiKey}`);
+setUpcomingMovies(results);
+
+};
+const fetchPlayingMovie= async()=>{
+  const{data:{results}}= await axios.get(`${url}/movie/${nowPlaying}?api_key=${apiKey}`);
+  setPlayingMovies(results);
+  };
+const fetchPopular= async()=>{
+  const{data:{results}}= await axios.get(`${url}/movie/${popular}?api_key=${apiKey}`);
+  setPopularMovies(results);
+  };
+  const fetchTopRatedMovie= async()=>{
+    const{data:{results}}= await axios.get(`${url}/movie/${topRated}?api_key=${apiKey}`);
+    setTopRatedMovies(results);
+    };
+fetchUpcoming();
+fetchPlayingMovie();
+fetchPopular();
+fetchTopRatedMovie();
+
+},[])
+
   return (
     <>
       <section className="home">
         <div className="banner"></div>
-        <Row title={"Popular on Netflix"} />
-        <Row title={"Recent Launch"} />
-        <Row title={"Action Movies"} />
-        <Row title={"Si-fi Movies"} />
+        <Row title={"Upcomings Movies"} arr={upcomingMovies} />
+        <Row title={"Top Rated Movies"} arr={topRatedMovies} />
+        <Row title={"Popular Movies"} arr={popularMovies} />
+        <Row title={"Now Playing Movies"} arr={playingMovies} />
+      
       </section>
     </>
   );
